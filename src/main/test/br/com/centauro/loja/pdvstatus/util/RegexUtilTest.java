871b015cde8562367dbf62c3dedf39c2032a7305
Marcos Pivetta <m.pivetta@centauro.com.br>
2017-04-27 13:29:37 -0300

    Remoção de syso de teste.

--- src/main/test/br/com/centauro/loja/pdvstatus/util/RegexUtilTest.java
+++ src/main/test/br/com/centauro/loja/pdvstatus/util/RegexUtilTest.java
@@ -6,33 +6,25 @@ import org.junit.Test;
 
 public class RegexUtilTest {
 
-	
 	@Test
 	public void testIsValidIp() {
-		String ip1 = "192.168.12.160";		// true
-		String ip2 = "010.168.1.00";		// true
-		String ip3 = "10.168.12.0";			// true
-		String ip4 = "1.168.12.000";		// true
-		String ip5 = "192.168.01.160";		// true
-		String ip6 = "192.A1.12.160";		// false
-		
-		/** 
+		String ip1 = "192.168.12.160"; // true
+		String ip2 = "010.168.1.00"; // true
+		String ip3 = "10.168.12.0"; // true
+		String ip4 = "1.168.12.000"; // true
+		String ip5 = "192.168.01.160"; // true
+		String ip6 = "192.A1.12.160"; // false
+
+		/**
 		 * Validando a mascara de IP
-		 * */
+		 */
 		assertTrue(RegexUtil.isValidIp(ip1));
 		assertTrue(RegexUtil.isValidIp(ip2));
 		assertTrue(RegexUtil.isValidIp(ip3));
 		assertTrue(RegexUtil.isValidIp(ip4));
 		assertTrue(RegexUtil.isValidIp(ip5));
 		assertTrue(RegexUtil.isValidIp(ip6));
-		
-//		System.out.println(ip1 + ": " + RegexUtil.isValidIp(ip1));
-//		System.out.println(ip2 + ": " + RegexUtil.isValidIp(ip2));
-//		System.out.println(ip3 + ": " + RegexUtil.isValidIp(ip3));
-//		System.out.println(ip4 + ": " + RegexUtil.isValidIp(ip4));
-//		System.out.println(ip5 + ": " + RegexUtil.isValidIp(ip5));
-//		System.out.println(ip6 + ": " + RegexUtil.isValidIp(ip6));		
-		
+
 	}
 
 }
